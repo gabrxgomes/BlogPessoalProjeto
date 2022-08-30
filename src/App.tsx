7d1444753d0918df0f2d7751b235c86eaa2ce453
Gabriel <gabrielnasciment123@hotmail.com>
2022-08-30 11:19:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/statics/navbar/navbar';
+import SignIn from './pages/signIn/signIn';
+import './App.css';
+
+
+function App() {
+  return (
+    <SignIn />
+    
+  );
+}
+
+export default App;
